9fddfa9256bac858c116adb5a16adc887116e5fa
Svein Are Danielsen <svein.danielsen@inmeta.no>
2020-09-04 10:22:16 +0200

    installed strapi, moved next-app into frontend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6b1cf43174df72d5fb490bc739839b2a'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4d0c76e5-af56-49f3-be8a-ac8813235c3c'
+};
